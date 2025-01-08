228de34f4a3d5642586643e874a443c1a80bdc5a
Sahu-Rachana <rachana.sahu@particle41.com>
2025-01-08 12:23:20 +0530

    Delete 6 User Order Management System/user-order-management-system directory

--- 6 User Order Management System/user-order-management-system/models/authModel.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../config/dbConnect'); 
-
-exports.register = (newUser, cb) => {
-    connection.query('INSERT INTO user_register SET ?', newUser, cb);
-};
-
-exports.login = (username, cb) => {
-    connection.query('SELECT * FROM user_register WHERE username = ? ', username, cb);
-};
